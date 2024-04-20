9066f166e0ba72fa95d9e1c722da6acabb6c89cf
Amr Abouelkhair <abouelkhair.305@gmail.com>
2024-04-20 22:50:13 +0000

    Update TGN Model and readme

--- README.md
+++ README.md
@@ -1,18 +1,37 @@
 # DGEval
-## Setting up the environment
+## TGN (We should run this first because it downloads the datasets)
+We draw heavy inspiration from the examples provided from the pytorch-geometrics library and we run it at our own convenience 
+We run each dataset 5 times and calculate the mean average precision and mean ROC AUC and their standard deviations
+### Setting up the environment
 We have set up our environment using conda and have provided a conda environment yaml file. 
 In order to replicate our environment please download conda from [here](https://conda.io/projects/conda/en/latest/user-guide/install/linux.html) and run 
 ```
+cd TGN
 conda env create -f environment.yml
 ```
-
-## Evaluating the models
-We have provided a python file for each model/dataset combination we did in order to run any of the evaluations please run 
+### Evaluating the models
+We have provided a python file for each dataset we did. in order to run any of the evaluations please run 
 ```
-python <model>-<dataset>.py
+python tgn>-<dataset>.py
 ```
 For example for the TGN model with the wikipedia dataset you can run 
 ```
 python tgn-wikipedia.py
 ```
+
+## TGAT 
+We rerun the implementation provided by the author. To rerun that example clone the repository of the author linked below.
+After running the TGN model you will find a directory called data with both the wikipedia and reddit dataset. In each direction there's a raw directory with a csv file in it. Just copy it to the processed dir in the TGAT repo and reproduce. 
+To re run also perform the following in the root directory of this repo
+```
+git clone git@github.com:StatsDLMathsRecomSys/Inductive-representation-learning-on-temporal-graphs.git
+cd Inductive-representation-learning-on-temporal-graphs
+cp ../data/JODIE/<dataset-name>/raw/<dataset-name>.csv
+# You have to uncomment the repo of your interest in the process.py file
+python process.py 
+python -u learn_edge.py -d wikipedia --bs 200 --uniform  --n_degree 20 --agg_method attn --attn_mode prod --gpu 0 --n_head 2 --prefix hello_world
+```
 ## References
+1. https://github.com/pyg-team/pytorch_geometric/blob/master/examples/tgn.py
+2. https://github.com/StatsDLMathsRecomSys/Inductive-representation-learning-on-temporal-graphs/blob/master/README.md
+3. https://github.com/xkcd1838/bench-DGNN/tree/299fee7dda057c6e9479bcb713afbc1a7c31b16b
---
+++ TGN/tgn-wikipedia.py
@@ -0,0 +1,185 @@
+# This code achieves a performance of around 96.60%. However, it is not
+# directly comparable to the results reported by the TGN paper since a
+# slightly different evaluation setup is used here.
+# In particular, predictions in the same batch are made in parallel, i.e.
+# predictions for interactions later in the batch have no access to any
+# information whatsoever about previous interactions in the same batch.
+# On the contrary, when sampling node neighborhoods for interactions later in
+# the batch, the TGN paper code has access to previous interactions in the
+# batch.
+# While both approaches are correct, together with the authors of the paper we
+# decided to present this version here as it is more realsitic and a better
+# test bed for future methods.
+
+import os.path as osp
+
+import numpy as np
+import torch
+from sklearn.metrics import average_precision_score, roc_auc_score
+from torch.nn import Linear
+
+from tgn_model import GraphAttentionEmbedding, LinkPredictor
+from torch_geometric.datasets import JODIEDataset
+from torch_geometric.loader import TemporalDataLoader
+from torch_geometric.nn import TGNMemory, TransformerConv
+from torch_geometric.nn.models.tgn import (
+    IdentityMessage,
+    LastAggregator,
+    LastNeighborLoader,
+)
+
+from tqdm import tqdm, trange
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'JODIE')
+dataset = JODIEDataset(path, name='wikipedia')
+data = dataset[0]
+
+# For small datasets, we can put the whole dataset on GPU and thus avoid
+# expensive memory transfer costs for mini-batches:
+data = data.to(device)
+
+train_data, val_data, test_data = data.train_val_test_split(
+    val_ratio=0.15, test_ratio=0.15)
+
+train_loader = TemporalDataLoader(
+    train_data,
+    batch_size=200,
+    neg_sampling_ratio=1.0,
+)
+val_loader = TemporalDataLoader(
+    val_data,
+    batch_size=200,
+    neg_sampling_ratio=1.0,
+)
+test_loader = TemporalDataLoader(
+    test_data,
+    batch_size=200,
+    neg_sampling_ratio=1.0,
+)
+neighbor_loader = LastNeighborLoader(data.num_nodes, size=10, device=device)
+
+memory_dim = time_dim = embedding_dim = 100
+
+memory = TGNMemory(
+    data.num_nodes,
+    data.msg.size(-1),
+    memory_dim,
+    time_dim,
+    message_module=IdentityMessage(data.msg.size(-1), memory_dim, time_dim),
+    aggregator_module=LastAggregator(),
+).to(device)
+
+gnn = GraphAttentionEmbedding(
+    in_channels=memory_dim,
+    out_channels=embedding_dim,
+    msg_dim=data.msg.size(-1),
+    time_enc=memory.time_enc,
+).to(device)
+
+link_pred = LinkPredictor(in_channels=embedding_dim).to(device)
+
+optimizer = torch.optim.Adam(
+    set(memory.parameters()) | set(gnn.parameters())
+    | set(link_pred.parameters()), lr=0.0001)
+criterion = torch.nn.BCEWithLogitsLoss()
+
+# Helper vector to map global node indices to local ones.
+assoc = torch.empty(data.num_nodes, dtype=torch.long, device=device)
+
+
+def train():
+    memory.train()
+    gnn.train()
+    link_pred.train()
+
+    memory.reset_state()  # Start with a fresh memory.
+    neighbor_loader.reset_state()  # Start with an empty graph.
+
+    total_loss = 0
+    for batch in train_loader:
+        optimizer.zero_grad()
+        batch = batch.to(device)
+
+        n_id, edge_index, e_id = neighbor_loader(batch.n_id)
+        assoc[n_id] = torch.arange(n_id.size(0), device=device)
+
+        # Get updated memory of all nodes involved in the computation.
+        z, last_update = memory(n_id)
+        z = gnn(z, last_update, edge_index, data.t[e_id].to(device),
+                data.msg[e_id].to(device))
+        pos_out = link_pred(z[assoc[batch.src]], z[assoc[batch.dst]])
+        neg_out = link_pred(z[assoc[batch.src]], z[assoc[batch.neg_dst]])
+
+        loss = criterion(pos_out, torch.ones_like(pos_out))
+        loss += criterion(neg_out, torch.zeros_like(neg_out))
+
+        # Update memory and neighbor loader with ground-truth state.
+        memory.update_state(batch.src, batch.dst, batch.t, batch.msg)
+        neighbor_loader.insert(batch.src, batch.dst)
+
+        loss.backward()
+        optimizer.step()
+        memory.detach()
+        total_loss += float(loss) * batch.num_events
+
+    return total_loss / train_data.num_events
+
+
+@torch.no_grad()
+def test(loader):
+    memory.eval()
+    gnn.eval()
+    link_pred.eval()
+
+    torch.manual_seed(12345)  # Ensure deterministic sampling across epochs.
+
+    aps, aucs = [], []
+    for batch in loader:
+        batch = batch.to(device)
+
+        n_id, edge_index, e_id = neighbor_loader(batch.n_id)
+        assoc[n_id] = torch.arange(n_id.size(0), device=device)
+
+        z, last_update = memory(n_id)
+        z = gnn(z, last_update, edge_index, data.t[e_id].to(device),
+                data.msg[e_id].to(device))
+        pos_out = link_pred(z[assoc[batch.src]], z[assoc[batch.dst]])
+        neg_out = link_pred(z[assoc[batch.src]], z[assoc[batch.neg_dst]])
+
+        y_pred = torch.cat([pos_out, neg_out], dim=0).sigmoid().cpu()
+        y_true = torch.cat(
+            [torch.ones(pos_out.size(0)),
+             torch.zeros(neg_out.size(0))], dim=0)
+
+        aps.append(average_precision_score(y_true, y_pred))
+        aucs.append(roc_auc_score(y_true, y_pred))
+
+        memory.update_state(batch.src, batch.dst, batch.t, batch.msg)
+        neighbor_loader.insert(batch.src, batch.dst)
+    return float(torch.tensor(aps).mean()), float(torch.tensor(aucs).mean())
+
+val_aps = []
+val_aucs = []
+test_aps = []
+test_aucs = []
+for run in trange(0, 1, desc="Runs"):
+    for epoch in trange(1, 51, desc="Epochs", leave=False):
+        loss = train()
+        tqdm.write(f'Epoch: {epoch:02d}, Loss: {loss:.4f}')
+        val_ap, val_auc = test(val_loader)
+        test_ap, test_auc = test(test_loader)
+        tqdm.write(f'Val AP: {val_ap:.4f}, Val AUC: {val_auc:.4f}')
+        tqdm.write(f'Test AP: {test_ap:.4f}, Test AUC: {test_auc:.4f}')
+
+    tqdm.write(f'Final Epoch: Val AP: {val_ap:.4f}, Val AUC: {val_auc:.4f}')
+    tqdm.write(f'Final Epoch: Test AP: {test_ap:.4f}, Test AUC: {test_auc:.4f}')
+    val_aps.append(val_ap)
+    val_aucs.append(val_aucs)
+    test_aps.append(test_ap)
+    test_aucs.append(test_auc)
+
+print('Average Metrics between runs')
+print(f'Val AP: {np.average(val_aps):.4f} +/- {np.std(val_aps):.4f}, Val AUC: {np.average(val_aucs):.4f} +/- {np.std(val_aucs):.4f}')
+print(f'Test AP: {np.average(test_aps):.4f} +/- {np.std(test_aps):.4f}, Test AUC: {np.average(test_aucs):.4f} +/- {np.std(test_aucs):.4f}')
